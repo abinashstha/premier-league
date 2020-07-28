b584c244ac93a75db6819ab5137eec0ce7a471f2
unknown <abinash.shrestha02@gmail.com>
2020-07-28 15:51:24 +0545

    autoprefixer and url loader

--- pl-frontend/webpack.config.js
+++ pl-frontend/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const autoprefixer = require('autoprefixer');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
@@ -12,9 +12,33 @@ module.exports = {
     module: {
         rules: [
             {
-                test:/\.js$/,
-                loader:'babel-loader',
-                exclude:/node_modules/     
-            }]
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                localIdentName: '[name]__[local]__[hash:base64:5]'
+                            }
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [ autoprefixer() ]
+                        }
+                    }
+                ]
+            }
+        ]
     }
 };
